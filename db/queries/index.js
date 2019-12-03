bb1bbbf24f4a37a247e01b43698dd67a7e64d2ef
M.Azhar Hussnain <34575273+miriiit@users.noreply.github.com>
2019-12-03 11:28:12 +0500

    Update index.js

--- db/queries/index.js
+++ db/queries/index.js
@@ -1,7 +1,4 @@
 
-const oracledb = require('oracledb');
-const db = require('../connect/oracledb.js');
-
 const util = require('../../utils')
 
 //Get All Collections
@@ -49,55 +46,6 @@ const queryUpdateProduct = async function (cb) {
 
 
 
-/*
-const queryDbInfo = async function (cb) {
-    let sql = "SELECT SYS_CONTEXT('userenv', 'sid') AS session_id FROM DUAL";
-    // Else everything looks good
-    // Obtain the Oracle Session ID, then return the connection
-    await db.doConnect(async (err, connection) => {
-        if (err) {
-            cb(err);
-        } else {
-            await db.doExecute(connection, sql, {}, async function (err, result) {
-                // Something went wrong, releae the connection and return the error
-                if (err) {
-                    console.log("ERROR: Unable to determine Oracle SESSION ID for this transaction: ", err);
-                    await releaseConnection(connection);
-                    cb(err);
-                }
-                // Log the connection ID (we do this to ensure the conncetions are being pooled correctly)
-                //console.log("INFO: Connection retrieved from the database, SESSION ID: ", result.rows[0]['SESSION_ID']);
-                cb(err, result.rows);
-            });
-        }
-    });
-};
-
-const querySystem = async function (cb) {
-    let sql = "select * from SYSTEM";
-    let queryRes = { err: null, res: null };
-    queryRes = await db.doConnect(async (err, connection) => {
-        if (err) {
-            if (db)
-                await db.doRelease(connection);
-            cb(err);
-        } else {
-            await db.doExecute(connection, sql, {}, async function (err, result) {
-                if (err) {
-                    if (db)
-                        db.doRelease(connection);     // RELEASE CONNECTION
-                    cb(err);             // ERROR
-                } else {
-                    if (db)
-                        db.doRelease(connection);     // RELEASE CONNECTION
-                    console.log(result.rows);
-                    cb(err, result.rows);
-                }
-            });
-        }
-    });
-};
-*/
 
 
 module.exports = {queryGetAllCollections, queryInsertCollection, queryUpdateCollection, queryGetAllProducts, queryInsertProduct, queryUpdateProduct };
